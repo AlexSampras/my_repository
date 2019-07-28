b5be60badc7219e02e9b47b7ab425d90225d063c
AlexSampras <f43322@olympus.ru>
2019-07-28 20:59:11 +0500

    Updated .gitignore

---
+++ src/com/dozmorovprojects/HelloWorld1.java
@@ -0,0 +1,8 @@
+package com.dozmorovprojects;
+
+public class HelloWorld1 {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/dozmorovprojects/HelloWorld2.java
@@ -0,0 +1,4 @@
+package com.dozmorovprojects;
+
+public class HelloWorld2 {
+}
